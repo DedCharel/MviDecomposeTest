103e56ee661a74610b080525755f714c878561ae
Daniil Shcherbakov <Charel@bk.ru>
2025-06-14 12:43:07 +0300

    StoreFactory Executor

--- app/src/main/java/com/example/mvidecomposetest/presentation/EditContactStoreFactory.kt
+++ app/src/main/java/com/example/mvidecomposetest/presentation/EditContactStoreFactory.kt
@@ -3,23 +3,54 @@ package com.example.mvidecomposetest.presentation
 import com.arkivanov.mvikotlin.core.store.Reducer
 import com.arkivanov.mvikotlin.core.store.Store
 import com.arkivanov.mvikotlin.core.store.StoreFactory
+import com.arkivanov.mvikotlin.extensions.coroutines.CoroutineExecutor
+import com.example.mvidecomposetest.domain.Contact
+import com.example.mvidecomposetest.domain.EditContactUseCase
 
 class EditContactStoreFactory(
-    private val storeFactory: StoreFactory
+    private val storeFactory: StoreFactory,
+    private val editContactUseCase: EditContactUseCase
 ) {
 
     private val store: Store<EditContactStore.Intent, EditContactStore.State, EditContactStore.Label> =
         storeFactory.create(
             name = "EditContactStore",
-            initialState = EditContactStore.State(username = "", phone = "")
+            initialState = EditContactStore.State(username = "", phone = ""),
+            reducer = ReducerImpl,
+            executorFactory =::ExecuteImpl
         )
 
     private sealed interface Action
+
+
 
     private sealed interface Msg {
         data class ChangeUsername(val username: String): Msg
 
         data class ChangePhone(val phone: String): Msg
+    }
+
+    private inner class ExecuteImpl: CoroutineExecutor<EditContactStore.Intent, Action,
+            EditContactStore.State, Msg, EditContactStore.Label>(){
+        override fun executeIntent(
+            intent: EditContactStore.Intent,
+            getState: () -> EditContactStore.State
+        ) {
+            when(intent){
+                is EditContactStore.Intent.ChangePhone -> {
+                    dispatch(Msg.ChangePhone(phone = intent.phone))
+                }
+                is EditContactStore.Intent.ChangeUsername -> {
+                    dispatch(Msg.ChangeUsername(username = intent.username))
+                }
+                EditContactStore.Intent.SaveContact -> {
+                    val state = getState()
+                    val contact = Contact(username = state.username, phone = state.phone)
+                    editContactUseCase(contact)
+                    publish(EditContactStore.Label.ContactSaved)
+                }
+            }
+        }
     }
 
     private object ReducerImpl: Reducer<EditContactStore.State, Msg>{
