5a0f556976306e4b5d81b5bd5cf086812ae79b63
Daniil Shcherbakov <Charel@bk.ru>
2025-06-16 08:39:31 +0300

    LoggingStoreFactory, refactoring

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,6 +79,7 @@ dependencies {
     implementation("com.arkivanov.decompose:extensions-compose-jetpack:2.2.2")
     implementation ("com.arkivanov.mvikotlin:mvikotlin:3.2.1")
     implementation ("com.arkivanov.mvikotlin:mvikotlin-main:3.2.1")
+    implementation ("com.arkivanov.mvikotlin:mvikotlin-logging:3.2.1")
     implementation("com.arkivanov.mvikotlin:mvikotlin-extensions-coroutines:3.2.1")
 
 }
